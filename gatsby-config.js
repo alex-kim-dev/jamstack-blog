0e5765da5467395d4b3856cf779c72da243c784f
Alex Kim <henvin.3@outlook.com>
2020-01-14 14:54:54 +0500

    Update gatsby config

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Jamstack blog`,
+    description: `Experimental blog built with react, gatsby, netlify & netlify cms.`,
+    author: `Alex Kim https://github.com/Alex-K1m`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -18,8 +18,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `Jamstack blog`,
+        short_name: `jamstack`,
         start_url: `/`,
         background_color: `#663399`,
         theme_color: `#663399`,
