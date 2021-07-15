6e1d27620313d3c084ecfa30e0b294691356dcd9
Alex Kim <45559664+Alex-K1m@users.noreply.github.com>
2021-07-15 13:44:36 +0500

    remove bundle analyser plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -37,12 +37,6 @@ module.exports = {
         openAnalyzer: false,
       },
     },
-    {
-      resolve: 'gatsby-plugin-netlify-cms',
-      options: {
-        modulePath: `${__dirname}/src/cms/cms.js`,
-      },
-    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
